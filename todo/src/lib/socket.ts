71330b0e1c41e4fc37392a208cbfa41257cff4d1
SpikeySanju <spikeysanju98@gmail.com>
2024-12-10 12:26:26 +0530

    use env

--- todo/src/lib/socket.ts
+++ todo/src/lib/socket.ts
@@ -1,11 +1,11 @@
 import { io } from 'socket.io-client';
+import { env } from '$env/dynamic/public';
 
 export function createSocketConnection(sessionToken: string) {
-	const socket = io('http://localhost:3000', {
+	const socket = io(env.PUBLIC_SOCKET_URL, {
 		auth: {
 			sessionToken
 		}
 	});
-
 	return socket;
 }
